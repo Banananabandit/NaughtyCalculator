c45d30711c5b5e6585ef0ea93e0311b3f69bd826
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-22 09:18:28 +0100

    Adding fuctionality for operators

--- app/src/main/java/android/banananabandit/naughtycalculator/MainActivity.kt
+++ app/src/main/java/android/banananabandit/naughtycalculator/MainActivity.kt
@@ -2,11 +2,16 @@ package android.banananabandit.naughtycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var numbersDisplay : TextView? = null
+
+    //These are the flags
+    var lastNumIsDot : Boolean = false
+    var lastNumIsNumber : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,40 +38,126 @@ class MainActivity : AppCompatActivity() {
         val buttonClear = findViewById<Button>(R.id.buttonClear)
         val buttonEnter = findViewById<Button>(R.id.buttonEnter)
 
+        //Operators
+        buttonMultiply.setOnClickListener{
+            // If the variable is a nullable then we always need to use the let- to make sure that the code only executes if its not null
+            numbersDisplay?.text?.let {
+                if (lastNumIsNumber && !isOperatorUsed(it.toString())){
+                    numbersDisplay?.append("x")
+                    lastNumIsNumber = false
+                    lastNumIsDot = false
+                }
+            }
+        }
+        buttonDivide.setOnClickListener {
+            numbersDisplay?.text?.let {
+                if (lastNumIsNumber && !isOperatorUsed(it.toString())){
+                    numbersDisplay?.append("/")
+                    lastNumIsNumber = false
+                    lastNumIsDot = false
+                }
+            }
+        }
+        buttonAdd.setOnClickListener {
+            numbersDisplay?.text?.let {
+                if (lastNumIsNumber && !isOperatorUsed(it.toString())){
+                    numbersDisplay?.append("+")
+                    lastNumIsNumber = false
+                    lastNumIsDot = false
+                }
+            }
+        }
+        buttonSubtract.setOnClickListener {
+            numbersDisplay?.text?.let {
+                if (lastNumIsNumber && !isOperatorUsed(it.toString())){
+                    numbersDisplay?.append("-")
+                    lastNumIsNumber = false
+                    lastNumIsDot = false
+                }
+            }
+        }
+
+
+        //Numbers
         buttonOne.setOnClickListener{
             numbersDisplay?.append("1")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonTwo.setOnClickListener{
             numbersDisplay?.append("2")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonThree.setOnClickListener{
             numbersDisplay?.append("3")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonFour.setOnClickListener{
             numbersDisplay?.append("4")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonFive.setOnClickListener{
             numbersDisplay?.append("5")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonSix.setOnClickListener{
             numbersDisplay?.append("6")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonSeven.setOnClickListener{
             numbersDisplay?.append("7")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonEight.setOnClickListener{
             numbersDisplay?.append("8")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonNine.setOnClickListener{
             numbersDisplay?.append("9")
+            lastNumIsNumber = true
+            lastNumIsDot = false
         }
         buttonZero.setOnClickListener{
             numbersDisplay?.append("0")
+            lastNumIsNumber = true
+            lastNumIsDot = false
+        }
+        buttonDot.setOnClickListener{
+            if (lastNumIsNumber && !lastNumIsDot){
+                if (!numbersDisplay!!.text.contains(".")){
+                    numbersDisplay?.append(".")
+                    lastNumIsNumber = false
+                    lastNumIsDot = true
+                }
+            }
         }
         buttonClear.setOnClickListener{
             numbersDisplay?.text = ""
         }
-
-
+        buttonDelete.setOnClickListener {
+            val deleteLastChar = numbersDisplay?.text?.dropLast(1)
+            numbersDisplay?.text = deleteLastChar
+            lastNumIsNumber = true
+            lastNumIsDot = false
+        }
     }
+    fun isOperatorUsed(value : String) : Boolean {
+        return if (value.startsWith("-")){
+            false
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+            // These will return true. The logic here is that it will allow us using negative numbers for the operations
+        }
+    }
+
 }
